f689951c99b0daa84571c29920e027930c51c71e
Manik Gupta <manikpokhetra@gmail.com>
2021-08-09 14:27:06 +0530

    Installed react router and variant bootstrap router Created a product screem and updated header and product link routes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import {Navbar,Nav,Container} from 'react-bootstrap';
+
+import { LinkContainer } from 'react-router-bootstrap';
 const Header = () => {
     return (
         <header>
            <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">
-                <i className="fab fa-shopify fa-2x"></i>ProShop</Navbar.Brand>
+            <LinkContainer to='/'>
+                <Navbar.Brand>               
+                    <i className="fab fa-shopify fa-2x"></i>ProShop
+                    </Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto">
-                <Nav.Link href="/cart"><i className="fas fa-shopping-cart fa-2x"></i> Cart</Nav.Link>
-                <Nav.Link href="/login"><i className="fas fa-user fa-2x"></i> Signin</Nav.Link>
+                <LinkContainer to='/cart'>
+                <Nav.Link>
+                    <i className="fas fa-shopping-cart fa-2x"></i> Cart
+                    </Nav.Link>
+                </LinkContainer>
+                <LinkContainer to='/login'>
+                <Nav.Link>
+                    <i className="fas fa-user fa-2x"></i> Signin
+                    </Nav.Link>
+                </LinkContainer>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
