29ac182a171631cad4da5bcc239b0965e49e5494
Manik Gupta <manikpokhetra@gmail.com>
2021-08-09 16:14:56 +0530

    Fetched products from backend using axios and hooks and rendered on homescreen and productsScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+// component level state for products fetching
 import{ Row, Col } from 'react-bootstrap';
-import products from '../products';
+// import products from '../products';
 import Product from '../components/Products';
+import axios from 'axios';
+// In a function component, we have no this, 
+// so we can’t assign or read this.state. Instead,
+//  we call the useState Hook directly inside our component:
 
 const HomeScreen = () => {
+    const [products,setProducts] = useState([]); //adding state
+
+useEffect(()=>{
+    const fetchProducts = async () =>{
+        const {data} = await axios.get('/api/products');
+        
+        setProducts(data);
+    }
+    fetchProducts();
+},[])
+
     return (
         <>
             <h1>Latest Products</h1>
